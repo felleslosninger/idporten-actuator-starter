df6ab1ae4a38892400fb6a1d98ab21d88fe203f8
Anne Marte Hjemås <kons-ahj@digdir.no>
2022-11-29 13:54:29 +0100

    MP-281: spring-boot 3.0 kompatibel autoconfig (#20)
    * spring-boot 3.0 kompatibel autoconfig

--- src/main/java/no/idporten/actuator/config/IDPortenActuatorsAutoConfiguration.java
+++ src/main/java/no/idporten/actuator/config/IDPortenActuatorsAutoConfiguration.java
@@ -2,13 +2,11 @@ package no.idporten.actuator.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@AutoConfiguration
 @ComponentScan(basePackages = {"no.idporten.actuator"})
-@PropertySource("classpath:/no/idporten/actuator/config/application-actuator.properties")
 public class IDPortenActuatorsAutoConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(IDPortenActuatorsAutoConfiguration.class);
